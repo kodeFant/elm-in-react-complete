c3735ffe4fdf1658a322761faed9be6064cdfafc
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2020-05-01 17:36:39 +0200

    Make Elm stateful

--- src/Elm/Main.elm
+++ src/Elm/Main.elm
@@ -1,13 +1,54 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
+import Html.Events exposing (onClick)
 
 
-main : Html msg
+main : Program () Model Msg
 main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( 0, Cmd.none )
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment Int
+    | Decrement Int
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment step ->
+            ( model + step, Cmd.none )
+
+        Decrement step ->
+            ( model - step, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
     div []
         [ h2 [] [ text "This is an Elm component" ]
-        , div [] [ text "Count: 0" ]
-        , button [] [ text "+" ]
-        , button [] [ text "-" ]
+        , div [] [ text ("Count: " ++ String.fromInt model) ]
+        , button [ onClick (Increment 1) ] [ text "+" ]
+        , button [ onClick (Decrement 1) ] [ text "-" ]
         ]
